9b371b1cba6b6acd739034acf9f7e02c74244194
GavinJZhu <102933876+GavinJZhu@users.noreply.github.com>
2025-10-04 11:30:03 -0700

    finishing guestbook

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -1,21 +1,49 @@
 package _00_Intro_To_ArrayLists;
 
+import java.util.ArrayList;
+
 public class _01_IntroToArrayLists {
     public static void main(String[] args) {
         // 1. Create an array list of Strings
         //    Don't forget to import the ArrayList class
-
+        ArrayList<String> strings = new ArrayList<String>();
         // 2. Add five Strings to your list
-
+            strings.add("One");
+            strings.add("Two");
+            strings.add("Three");
+            strings.add("Four");
+            strings.add("Five");
         // 3. Print all the Strings using a standard for-loop
-
+        System.out.println("consecutive order, unchanged");
+        for (int i = 0; i < strings.size(); i++) {
+            String s = strings.get(i);
+            System.out.println(s);
+        }
         // 4. Print all the Strings using a for-each loop
-
+        System.out.println("consecutive order, printed for for-each");
+        for(String s : strings){
+            System.out.println(s);
+        }
         // 5. Print only the even numbered elements in the list.
-
+        System.out.println("only even indexed elements");
+        for (int i = 0; i < strings.size(); i++) {
+            if (i%2 == 0) {
+                String s = strings.get(i);
+                System.out.println(s);
+            }
+        }
         // 6. Print all the Strings in reverse order.
-
+        System.out.println("reversed");
+        for (int i = strings.size()-1; i >= 0; i--) {
+                String s = strings.get(i);
+                System.out.println(s);
+        }
         // 7. Print only the Strings that have the letter 'e' in them.
-        
+        System.out.println("only with the letter e");
+        for (String s : strings) {
+            if (s.contains("e")) {
+                System.out.println(s);
+            }
+        }
     }
 }
--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,14 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class _02_GuestBook implements ActionListener {
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
@@ -13,5 +21,50 @@ public class _02_GuestBook {
      * Guest #3: Greg Ganders
      * Guest #4: Donny Doners
      */
+    static ArrayList<String> names = new ArrayList<String>();
+    static JFrame frame = new JFrame();
+    static JPanel panel = new JPanel();
+    static JButton addName = new JButton();
+    static JButton viewNames = new JButton();
 
+    public static void main(String[] args) {
+
+        names.add(formatNames("Bob Banders", 1));
+        names.add(formatNames("Sandy Summers",2));
+        names.add("Greg Ganders");
+        names.add("Donny Donners");
+
+        viewNames.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, names.toArray());
+
+            }
+        });
+        addName.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = JOptionPane.showInputDialog(null, "Input a name into the guestbook here.");
+                names.add(name);
+            }
+        });
+        frame.setSize(100, 100);
+        frame.add(panel);
+        panel.add(addName);
+        panel.add(viewNames);
+        viewNames.setText("View Names");
+        addName.setText("Add Name");
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    // Guest #1: Bob Banders
+    public static String formatNames(String name, int index){
+        String formattedName;
+        formattedName = "Guest #"+index+":"+" "+name;
+        return formattedName;
+    }
 }
