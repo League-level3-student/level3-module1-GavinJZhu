5241b8fb8ae9d769809f414aa26f5554dfafeb63
GavinJZhu <102933876+GavinJZhu@users.noreply.github.com>
2025-11-15 11:49:55 -0800

    im disoriented

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,9 +1,14 @@
 package _06_Intro_To_Hash_Maps;
 
 
-public class _02_LogSearch {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+public class _02_LogSearch implements ActionListener {
     /*
-     * Crate a HashMap of Integers for the keys and Strings for the values.
+     * Create a HashMap of Integers for the keys and Strings for the values.
      * Create a GUI with three buttons.
      * Button 1: Add Entry
      *      When this button is clicked, use an input dialog to ask the user
@@ -33,5 +38,57 @@ public class _02_LogSearch {
      *      If this ID exists in the HashMap, remove it. Otherwise, notify the
      *      user that the ID is not in the list.
      */
+    static HashMap<Integer, String> ids = new HashMap<Integer, String>();
+    static JFrame frame = new JFrame();
+    static JPanel panel = new JPanel();
+    static JButton add = new JButton();
+    static JButton search = new JButton();
+    static JButton view = new JButton();
+    static JButton remove = new JButton();
 
+    public static void main(String[] args) {
+        panel.add(add);
+        add.setText("Add Entry");
+        panel.add(search);
+        search.setText("Search by ID");
+        panel.add(view);
+        view.setText("View List");
+        panel.add(remove);
+        remove.setText("Remove Entry");
+        frame.add(panel);
+        frame.setVisible(true);
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String idNumber = JOptionPane.showInputDialog("Please enter your id number. (only use numbers from the hindu-arabic numeral system)", null);
+                String name = JOptionPane.showInputDialog("Please enter your name.", null);
+                ids.put(Integer.valueOf(idNumber),name);
+                JOptionPane.showMessageDialog(null, "Thanks! You can close this window now.");
+            }
+        });
+        search.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id = JOptionPane.showInputDialog("Enter ID", null);
+
+            }
+        });
+        view.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        remove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
