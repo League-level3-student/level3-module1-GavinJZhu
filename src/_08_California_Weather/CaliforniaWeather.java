26df5edd5ca7ab13413ed4fd4f3f0c2e36312c20
GavinJZhu <102933876+GavinJZhu@users.noreply.github.com>
2025-11-30 12:56:09 -0800

    part way through with california weather

--- src/_08_California_Weather/CaliforniaWeather.java
+++ src/_08_California_Weather/CaliforniaWeather.java
@@ -1,5 +1,8 @@
 package _08_California_Weather;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 /*
@@ -28,14 +31,55 @@ import java.util.HashMap;
  */
 
 public class CaliforniaWeather {
-    
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JButton citySearch = new JButton();
+    JButton weatherSearch = new JButton();
+    JButton tempRangeSearch = new JButton();
     void start() {
         HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
         
         // All city keys have the first letter capitalized of each word
         String cityName = Utilities.capitalizeWords( "National City" );
         WeatherData datum = weatherData.get(cityName);
-        
+
+        frame.setSize(100, 100);
+        citySearch.setText("City Search");
+        weatherSearch.setText("Weather Search");
+        tempRangeSearch.setText("Temperature Range Search");
+        panel.add(citySearch);
+        panel.add(weatherSearch);
+        panel.add(tempRangeSearch);
+        frame.add(panel);
+        frame.setVisible(true);
+        citySearch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String city = JOptionPane.showInputDialog("Input the city you'd like to search.");
+                String cityCapitalized = Utilities.capitalizeWords(city);
+                String summary = weatherData.get(cityCapitalized).weatherSummary;
+                if (summary == null){
+                    JOptionPane.showMessageDialog(null, "No city found with that name.");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, cityCapitalized +" is "+summary+" with a temperature of "+weatherData.get(cityCapitalized).temperatureF+"F");
+                }
+            }
+        });
+        weatherSearch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String city = JOptionPane.showInputDialog("Input the city you'd like to search.");
+
+            }
+        });
+        tempRangeSearch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
         if( datum == null ) {
             System.out.println("Unable to find weather data for: " + cityName);
         } else {
